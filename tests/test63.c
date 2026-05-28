d5bd926b57116e31da83756b1fcfd7f5bfa02d7a
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2026-05-28 11:35:33 -0400

    utlist: Exhaustive white-box testing of {LL,DL}_CONCAT. NFC.
    Commit 6346342267 changed test86.c to be more "white-box" about
    what actually happens to the prev and next fields during an
    {LL,DL,CDL}_APPEND. This commit does the same to test63.c and
    test64.c, for {LL,DL}_CONCAT, and merges them into one test file.

--- tests/test63.c
+++ tests/test63.c
@@ -1,67 +1,94 @@
 #include <stdio.h>
 #include "utlist.h"
 
-typedef struct el {
-    int id;
-    struct el *next, *prev;
-} el;
+struct List {
+    int value;
+    struct List *prev;
+    struct List *next;
+};
+
+struct List dummy = {9, NULL, NULL};
+
+static void dump_els(struct List *list1, struct List *list2, struct List *els, int n) {
+    for (int i=0; i < n; ++i) {
+        printf("%d (prev %d next %d)\n", els[i].value,
+            els[i].prev ? els[i].prev->value : -1,
+            els[i].next ? els[i].next->value : -1);
+    }
+}
+
+static void test_ll_concat_case(int a, int b) {
+    struct List els[] = {
+        {1, &dummy, &dummy},
+        {2, &dummy, &dummy},
+        {3, &dummy, &dummy},
+        {4, &dummy, &dummy},
+        {5, &dummy, &dummy},
+        {6, &dummy, &dummy},
+    };
+    struct List *list1 = NULL;
+    struct List *list2 = NULL;
+
+    for (int i = 0; i < a; ++i) {
+        LL_APPEND(list1, &els[i]);
+    }
+    for (int i = 0; i < b; ++i) {
+        LL_APPEND(list2, &els[i+3]);
+    }
+    LL_CONCAT(list1, list2);
+
+    printf("### %d %d\n", a, b);
+    printf("list1 = %d, list2 = %d\n", list1 ? list1->value : -1, list2 ? list2->value : -1);
+    dump_els(list1, list2, els, 6);
+}
+
+static void test_ll_concat() {
+    printf("## test_ll_concat\n");
+    for (int a = 0; a <= 2; ++a) {
+        for (int b = 0; b <= 2; ++b) {
+            test_ll_concat_case(a, b);
+        }
+    }
+}
+
+static void test_dl_concat_case(int a, int b) {
+    struct List els[] = {
+        {1, &dummy, &dummy},
+        {2, &dummy, &dummy},
+        {3, &dummy, &dummy},
+        {4, &dummy, &dummy},
+        {5, &dummy, &dummy},
+        {6, &dummy, &dummy},
+    };
+    struct List *list1 = NULL;
+    struct List *list2 = NULL;
+
+    for (int i = 0; i < a; ++i) {
+        DL_APPEND(list1, &els[i]);
+    }
+    for (int i = 0; i < b; ++i) {
+        DL_APPEND(list2, &els[i+3]);
+    }
+    DL_CONCAT(list1, list2);
+
+    printf("### %d %d\n", a, b);
+    printf("list1 = %d, list2 = %d\n", list1 ? list1->value : -1, list2 ? list2->value : -1);
+    dump_els(list1, list2, els, 6);
+}
+
+static void test_dl_concat() {
+    printf("## test_dl_concat\n");
+    for (int a = 0; a <= 3; ++a) {
+        for (int b = 0; b <= 3; ++b) {
+            test_dl_concat_case(a, b);
+        }
+    }
+}
 
 int main()
 {
-    int i;
-    el els[10], *e;
-    el *headA = NULL;
-    el *headB = NULL;
-    for(i=0; i<10; i++) {
-        els[i].id=(int)'a'+i;
-    }
-
-    /* test LL macros */
-    printf("LL macros\n");
-    LL_APPEND(headA,&els[0]);
-    LL_APPEND(headA,&els[1]);
-    LL_APPEND(headA,&els[2]);
-    LL_FOREACH(headA,e) {
-        printf("%c ", e->id);
-    }
-    printf("\n");
-
-    LL_APPEND(headB,&els[3]);
-    LL_APPEND(headB,&els[4]);
-    LL_APPEND(headB,&els[5]);
-    LL_FOREACH(headB,e) {
-        printf("%c ", e->id);
-    }
-    printf("\n");
-
-    LL_CONCAT(headA,headB);
-    LL_FOREACH(headA,e) {
-        printf("%c ", e->id);
-    }
-    printf("\n");
-
-    /* other variations */
-    headA = NULL;
-    LL_CONCAT(headA,headB);
-    LL_FOREACH(headA,e) {
-        printf("%c ", e->id);
-    }
-    printf("\n");
-    headB = NULL;
-    LL_CONCAT(headA,headB);
-    LL_FOREACH(headA,e) {
-        printf("%c ", e->id);
-    }
-    printf("\n");
-    headA=NULL;
-    headB=NULL;
-    LL_APPEND(headA,&els[0]);
-    LL_APPEND(headB,&els[1]);
-    LL_CONCAT(headA,headB);
-    LL_FOREACH(headA,e) {
-        printf("%c ", e->id);
-    }
-    printf("\n");
+    test_ll_concat();
+    test_dl_concat();
 
     return 0;
 }
